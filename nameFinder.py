d1ae0ee84f5f71674e9ba08313cc431da394b479
Spencer Kee <spencerkee7@gmail.com>
2016-04-13 01:13:09 -0400

    Neatened reading of names and uid, removed error if person has 0 or >1 givenName
    Switched from using givenName to cn

--- nameFinder.py
+++ nameFinder.py
@@ -1,31 +1,28 @@
 emailLocation = 'emailAddresses'
 
 def write_ldap_data(filename): #some people have multiple give names
-
 	with open(filename) as f: 
 		data = f.read()
 
 	data = data.split('\n\n')
-	print len(data)
+	# print len(data)
 	new_data = []
 	valid_years = ['12', '13', '14', '15', '17']
 
 	for i in data:
 		data_block = str(i)
 		data_block = data_block.split('\n')
-		personal_information = []
+		personal_information = [[],[]]
 		for j in data_block:
-			if j[:4] == 'uid:' and j[len(j)-2:] in valid_years:
-				personal_information.append(j.split(': ')[1])
-			if 'givenName' in j:
-				personal_information.append(j.split(': ')[1])
-		if len(personal_information) >= 2:
+			if 'uid:' in j and j[len(j)-2:] in valid_years:
+				personal_information[0] = j.split(': ')[1]
+			if 'cn:' in j:
+				personal_information[1] = j.split(': ')[1]
+		if not any(x == [] for x in personal_information) and len(personal_information) >= 2:
 			new_data.append(personal_information)
 
 	print len(new_data)
-	for i in range(len(new_data)):
-		new_data[i][1] = new_data[i][1] + ' ' + new_data[i][0][1:3][0].upper() + new_data[i][0][2:len(new_data[i][0])-2]
-	print new_data
+
 	open(emailLocation, 'w').close()
 	with open(emailLocation, 'a') as f:
 		for i in new_data:
@@ -38,13 +35,16 @@ def main(first, second, year):
 	matching_emails = list(all_emails)
 	if first != '*':
 		matching_emails = [x for x in matching_emails if x[0] == first]
+	print len(matching_emails)
 	if second != '*':
 		matching_emails = [x for x in matching_emails if x[1] == second]
+	print len(matching_emails)
 	if year != '*':
 		matching_emails = [x for x in matching_emails if x[len(x)-2:] == year]
+	print len(matching_emails)
 	for i in matching_emails:
 		print i, emails_names[all_emails.index(i)][1]
 
 if __name__ == '__main__':
-	# main('*','k','*')
 	write_ldap_data('ldap_data')
+	main('s','k','*')
