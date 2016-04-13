ab25bc717afc30616e305b839a625fafc9541a42
Spencer Kee <spencerkee7@gmail.com>
2016-04-13 00:51:06 -0400

    Added function to convert ldap data into readable format.
    Converts long ldap blocks for every person into 'uid', 'name'.

--- nameFinder.py
+++ nameFinder.py
@@ -1,5 +1,37 @@
 emailLocation = 'emailAddresses'
-nameLocation = 'allNames'
+
+def write_ldap_data(filename): #some people have multiple give names
+
+	with open(filename) as f: 
+		data = f.read()
+
+	data = data.split('\n\n')
+	print len(data)
+	new_data = []
+	valid_years = ['12', '13', '14', '15', '17']
+
+	for i in data:
+		data_block = str(i)
+		data_block = data_block.split('\n')
+		personal_information = []
+		for j in data_block:
+			if j[:4] == 'uid:' and j[len(j)-2:] in valid_years:
+				personal_information.append(j.split(': ')[1])
+			if 'givenName' in j:
+				personal_information.append(j.split(': ')[1])
+		if len(personal_information) >= 2:
+			new_data.append(personal_information)
+
+	print len(new_data)
+	for i in range(len(new_data)):
+		new_data[i][1] = new_data[i][1] + ' ' + new_data[i][0][1:3][0].upper() + new_data[i][0][2:len(new_data[i][0])-2]
+	print new_data
+	open(emailLocation, 'w').close()
+	with open(emailLocation, 'a') as f:
+		for i in new_data:
+			string = i[0] + ', '+ i[1] + '\n'
+			f.write(string)
+
 def main(first, second, year):
 	with open(emailLocation) as f: emails_names = [x.strip('\n').split(',') for x in f]
 	all_emails = [x[0] for x in emails_names]
@@ -14,4 +46,5 @@ def main(first, second, year):
 		print i, emails_names[all_emails.index(i)][1]
 
 if __name__ == '__main__':
-	main('*','k','*')
+	# main('*','k','*')
+	write_ldap_data('ldap_data')
